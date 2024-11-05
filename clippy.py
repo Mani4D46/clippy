099fa7253c2bf45e1db7a4c2e9e052d03fb296f9
Rizen54 <psatvik54@proton.me>
2024-11-05 15:29:02 +0530

    All funcs functional with arguements

--- clippy.py
+++ clippy.py
@@ -25,8 +25,21 @@ def display_bookmarks():
     return bookmarks
 
 
+def add_bookmark(key, value):
+    bookmarks = load_bookmarks()
+    
+    if key in bookmarks:
+        print(f"Key '{key}' already exists. Use a different key.")
+        return
+
+    bookmarks[key] = value
+    with open(config_path, "w") as file:
+        json.dump(bookmarks, file, indent=4)  # Save to file
+    print(f"Bookmark added: {key} -> {value}")
+
+
 # Example of copying a bookmarked item to clipboard
-def copy_bookmark_to_clipboard(key):
+def copy_bookmark(key):
     bookmarks = load_bookmarks()
     if key in bookmarks:
         pyperclip.copy(bookmarks[key])
@@ -38,5 +51,19 @@ def copy_bookmark_to_clipboard(key):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--display", help="display available keys in the clipboard", action="store_true")
-    parser.add_argument('-a', '--add', nargs=2, metavar=('KEY', 'VALUE'), help="Add a bookmark with key and value")
-    parser.parse_args()
+    parser.add_argument('-a', '--add', nargs=1, metavar=('KEY'), help="Add a bookmark with key and auto recognized pasted content")
+    parser.add_argument('-b', '--bookmark', nargs=2, metavar=('KEY', 'VALUE'), help="Add a bookmark with key and self entered value")
+    parser.add_argument('-c', '--copy', nargs=1, metavar=('KEY'), help="Copy the value of a selected key")
+    args = parser.parse_args()
+
+    if args.display:
+        display_bookmarks()
+    elif args.add:
+        key = ', '.join(map(str, args.add))  # Join list elements into a single string
+        value = str(pyperclip.paste())
+    elif args.bookmark:
+        key, value = args.bookmark
+        add_bookmark(key, value)
+    elif args.copy:
+        key = ', '.join(map(str, args.copy))
+        copy_bookmark(key)
