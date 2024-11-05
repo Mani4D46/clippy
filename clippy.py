b8facdf34bdd74044461372ac71ca78ea4557309
Rizen54 <psatvik54@proton.me>
2024-11-05 22:40:53 +0530

    Added colors and nerd fonts

--- clippy.py
+++ clippy.py
@@ -2,6 +2,7 @@ import argparse
 import pyperclip
 import os
 import json
+from termcolor import colored
 
 config_path = os.path.join(os.path.expanduser("~"), ".config", "clippy.json")
 
@@ -21,7 +22,7 @@ def load_bookmarks():
 def display_bookmarks():
     bookmarks = load_bookmarks()
     for key, value in bookmarks.items():
-        print(f"{key}: {value}")
+        print(f"{colored(key, "red")}: {colored(value, "yellow")}")
     return bookmarks
 
 
@@ -29,13 +30,13 @@ def add_bookmark(key, value):
     bookmarks = load_bookmarks()
     
     if key in bookmarks:
-        print(f"Key '{key}' already exists. Use a different key.")
+        print(f"{colored("󱫈", "red")} Key '{key}' already exists. Use a different key.")
         return
 
     bookmarks[key] = value
     with open(config_path, "w") as file:
         json.dump(bookmarks, file, indent=4)  # Save to file
-    print(f"Bookmark added: {key} -> {value}")
+    print(f"{colored("󱫉", "green")} Bookmark added: {key} -> {value}")
 
 
 # Example of copying a bookmarked item to clipboard
@@ -43,9 +44,9 @@ def copy_bookmark(key):
     bookmarks = load_bookmarks()
     if key in bookmarks:
         pyperclip.copy(bookmarks[key])
-        print(f"Copied {key}: {bookmarks[key]} to clipboard")
+        print(f"{colored("󱫆", "green")} Copied {key}: {bookmarks[key]} to clipboard")
     else:
-        print(f"No bookmark found for '{key}'")
+        print(f"{colored("󱫊", "red")} No bookmark found for '{key}'")
 
 
 if __name__ == "__main__":
@@ -57,10 +58,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.display:
+        print(colored(" Here is your clipboard:", "green"))
         display_bookmarks()
     elif args.add:
         key = ', '.join(map(str, args.add))  # Join list elements into a single string
         value = str(pyperclip.paste())
+        add_bookmark(key, value)
     elif args.bookmark:
         key, value = args.bookmark
         add_bookmark(key, value)
